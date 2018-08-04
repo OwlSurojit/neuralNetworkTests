93b688d0e0838f99ca39281c577c7c4d8dbfad86
OwlSurojit <surojit@gmx.de>
2018-08-04 19:23:13 +0200

    format
    for the beauty... ;)

--- src/mainClass/Matrix.java
+++ src/mainClass/Matrix.java
@@ -2,163 +2,160 @@ package mainClass;
 
 public class Matrix {
 
-  int rows, cols, digitsAfterComma;
-  double[][] arr;
+	int rows, cols, digitsAfterComma;
+	double[][] arr;
 
-  public Matrix(int rows, int cols) {
-    this.rows = rows; 
-    this.cols = cols;
-    this.arr = new double[rows][cols];
-  }
+	public Matrix(int rows, int cols) {
+		this.rows = rows;
+		this.cols = cols;
+		this.arr = new double[rows][cols];
+	}
 
-  public void define(double[] def) {
-    for (int i=0; i<this.rows; i++) {
-      for (int j=0; j<this.cols; j++) {
-        this.arr[i][j] = def[i*cols+j];
-      }
-    }
-  }
+	public void define(double[] def) {
+		for (int i = 0; i < this.rows; i++) {
+			for (int j = 0; j < this.cols; j++) {
+				this.arr[i][j] = def[i * cols + j];
+			}
+		}
+	}
 
-  public void randomise(double lowest, double highest, int digitsAfterComma) {
-    this.digitsAfterComma = digitsAfterComma;
-    for (int i=0; i<this.rows; i++) {
-      for (int j=0; j<this.cols; j++) {
-        this.arr[i][j] = roundDigits((double) (Math.random()*highest+lowest), this.digitsAfterComma);
-      }
-    }
-  }
+	public void randomise(double lowest, double highest, int digitsAfterComma) {
+		this.digitsAfterComma = digitsAfterComma;
+		for (int i = 0; i < this.rows; i++) {
+			for (int j = 0; j < this.cols; j++) {
+				this.arr[i][j] = roundDigits((double) (Math.random() * highest + lowest), this.digitsAfterComma);
+			}
+		}
+	}
 
-  public void scl(double scale) {
-    for (int i=0; i<this.rows; i++) {
-      for (int j=0; j<this.cols; j++) {
-        this.arr[i][j] *= scale;
-      }
-    }
-  }
+	public void scl(double scale) {
+		for (int i = 0; i < this.rows; i++) {
+			for (int j = 0; j < this.cols; j++) {
+				this.arr[i][j] *= scale;
+			}
+		}
+	}
 
-  public Matrix transpose() {
-    Matrix transposed = new Matrix(this.cols, this.rows);
-    for (int i=0; i<this.rows; i++) {
-      for (int j=0; j<this.cols; j++) {
-        transposed.arr[j][i] = this.arr[i][j];
-      }
-    }
-    return transposed;
-  }
+	public Matrix transpose() {
+		Matrix transposed = new Matrix(this.cols, this.rows);
+		for (int i = 0; i < this.rows; i++) {
+			for (int j = 0; j < this.cols; j++) {
+				transposed.arr[j][i] = this.arr[i][j];
+			}
+		}
+		return transposed;
+	}
 
-  public void debug() {
-    for (double[] i : this.arr) {
-      System.out.print("[  ");
-      for (double j : i) {
-        System.out.print(roundDigits(j, this.digitsAfterComma) + "  ");
-      }
-      System.out.print("]\n");
-    }
-    System.out.println();
-  }
+	public void debug() {
+		for (double[] i : this.arr) {
+			System.out.print("[  ");
+			for (double j : i) {
+				System.out.print(roundDigits(j, this.digitsAfterComma) + "  ");
+			}
+			System.out.print("]\n");
+		}
+		System.out.println();
+	}
 
-  public void sigmoidEach() {
-    for (int i=0; i<this.rows; i++) {
-      for (int j=0; j<this.cols; j++) {
-        this.arr[i][j] = sigmoid(this.arr[i][j]);
-      }
-    }
-  }
+	public void sigmoidEach() {
+		for (int i = 0; i < this.rows; i++) {
+			for (int j = 0; j < this.cols; j++) {
+				this.arr[i][j] = sigmoid(this.arr[i][j]);
+			}
+		}
+	}
 
-  public Matrix dSigmoidEach() {
-    Matrix result = new Matrix(this.rows, this.cols);
-    for (int i=0; i<this.rows; i++) {
-      for (int j=0; j<this.cols; j++) {
-        result.arr[i][j] = dSigmoid(this.arr[i][j]);
-      }
-    }
-    return result;
-  }
+	public Matrix dSigmoidEach() {
+		Matrix result = new Matrix(this.rows, this.cols);
+		for (int i = 0; i < this.rows; i++) {
+			for (int j = 0; j < this.cols; j++) {
+				result.arr[i][j] = dSigmoid(this.arr[i][j]);
+			}
+		}
+		return result;
+	}
 
-  public void add(Matrix b) throws IllegalArgumentException {
-    if (this.rows!=b.rows || this.cols!=b.cols) {
-      throw new IllegalArgumentException("The matricies to add haven't got the same number of rows and columns!");
-    }
-    for (int i=0; i<this.rows; i++) {
-      for (int j=0; j<this.cols; j++) {
-        this.arr[i][j] += b.arr[i][j];
-      }
-    }
-  }
+	public void add(Matrix b) throws IllegalArgumentException {
+		if (this.rows != b.rows || this.cols != b.cols) {
+			throw new IllegalArgumentException("The matricies to add haven't got the same number of rows and columns!");
+		}
+		for (int i = 0; i < this.rows; i++) {
+			for (int j = 0; j < this.cols; j++) {
+				this.arr[i][j] += b.arr[i][j];
+			}
+		}
+	}
 
-  public void multiply(Matrix b) {
-    // hadamard product
-    for (int i=0; i<this.rows; i++) {
-      for (int j=0; j<this.cols; j++) {
-        this.arr[i][j] *= b.arr[i][j];
-      }
-    }
-  }
+	public void multiply(Matrix b) {
+		// hadamard product
+		for (int i = 0; i < this.rows; i++) {
+			for (int j = 0; j < this.cols; j++) {
+				this.arr[i][j] *= b.arr[i][j];
+			}
+		}
+	}
 
-  private double roundDigits(double num, int digitsAfterComma) {
-    int factor = (int) Math.pow(10, digitsAfterComma);
-    num = Math.round(num*factor);
-    num/=factor;
-    return num;
-  }
+	private double roundDigits(double num, int digitsAfterComma) {
+		int factor = (int) Math.pow(10, digitsAfterComma);
+		num = Math.round(num * factor);
+		num /= factor;
+		return num;
+	}
 
+	public static Matrix add(Matrix a, Matrix b) throws IllegalArgumentException {
+		if (a.rows != b.rows || a.cols != b.cols) {
+			throw new IllegalArgumentException("The matricies to add haven't got the same number of rows and columns!");
+		}
+		Matrix result = new Matrix(a.rows, a.cols);
+		result.digitsAfterComma = a.digitsAfterComma;
+		for (int i = 0; i < a.rows; i++) {
+			for (int j = 0; j < a.cols; j++) {
+				result.arr[i][j] = a.arr[i][j] + b.arr[i][j];
+			}
+		}
+		return result;
+	}
 
-public static Matrix add(Matrix a, Matrix b) throws IllegalArgumentException {
-  if (a.rows!=b.rows || a.cols!=b.cols) {
-    throw new IllegalArgumentException("The matricies to add haven't got the same number of rows and columns!");
-  }
-  Matrix result = new Matrix(a.rows, a.cols);
-  result.digitsAfterComma = a.digitsAfterComma;
-  for (int i=0; i<a.rows; i++) {
-    for (int j=0; j<a.cols; j++) {
-      result.arr[i][j] = a.arr[i][j] + b.arr[i][j];
-    }
-  }
-  return result;
-}
+	public static Matrix substract(Matrix a, Matrix b) throws IllegalArgumentException {
+		if (a.rows != b.rows || a.cols != b.cols) {
+			throw new IllegalArgumentException("The matricies haven't got the same number of rows and columns!");
+		}
+		Matrix result = new Matrix(a.rows, a.cols);
+		result.digitsAfterComma = a.digitsAfterComma;
+		for (int i = 0; i < a.rows; i++) {
+			for (int j = 0; j < a.cols; j++) {
+				result.arr[i][j] = a.arr[i][j] - b.arr[i][j];
+			}
+		}
+		return result;
+	}
 
+	public static Matrix multiply(Matrix a, Matrix b) throws IllegalArgumentException {
+		if (a.cols != b.rows) {
+			throw new IllegalArgumentException(
+					"The first matrix' number of columns isn't equal to the second matrix' number of rows!");
+		}
+		Matrix result = new Matrix(a.rows, b.cols);
+		result.digitsAfterComma = a.digitsAfterComma;
+		for (int i = 0; i < result.rows; i++) {
+			for (int j = 0; j < result.cols; j++) {
+				for (int k = 0; k < a.cols; k++) {
+					result.arr[i][j] += a.arr[i][k] * b.arr[k][j];
+				}
+			}
+		}
+		return result;
+	}
 
-public static Matrix substract(Matrix a, Matrix b) throws IllegalArgumentException {
-  if (a.rows!=b.rows || a.cols!=b.cols) {
-    throw new IllegalArgumentException("The matricies haven't got the same number of rows and columns!");
-  }
-  Matrix result = new Matrix(a.rows, a.cols);
-  result.digitsAfterComma = a.digitsAfterComma;
-  for (int i=0; i<a.rows; i++) {
-    for (int j=0; j<a.cols; j++) {
-      result.arr[i][j] = a.arr[i][j] - b.arr[i][j];
-    }
-  }
-  return result;
-}
+	public static double sigmoid(double x) {
+		return (1 / (1 + Math.exp(-x)));
+	}
 
+	public static double dSigmoid(double x) {
+		// sigmoid'(x) = sigmoid(x) * (1 - sigmoid(x))
+		// return sigmoid(x) * (1 - sigmoid(x)) ;
 
-public static Matrix multiply(Matrix a, Matrix b) throws IllegalArgumentException {
-  if (a.cols!=b.rows) {
-    throw new IllegalArgumentException("The first matrix' number of columns isn't equal to the second matrix' number of rows!");
-  }
-  Matrix result = new Matrix(a.rows, b.cols);
-  result.digitsAfterComma = a.digitsAfterComma;
-  for (int i=0; i<result.rows; i++) {
-    for (int j=0; j<result.cols; j++) {
-      for (int k=0; k<a.cols; k++) {
-        result.arr[i][j]+=a.arr[i][k]*b.arr[k][j];
-      }
-    }
-  }
-  return result;
-}
-
-
-public static double sigmoid(double x) {
-  return (1/(1 + Math.exp(-x)));
-}
-
-public static double dSigmoid(double x) {
-  //sigmoid'(x) = sigmoid(x) * (1 - sigmoid(x))
-  //return sigmoid(x) * (1 - sigmoid(x)) ;
-
-  //but here the output is already "sigmoided", therefore:
-  return x * (1 - x);
-}
+		// but here the output is already "sigmoided", therefore:
+		return x * (1 - x);
+	}
 }
--- src/mainClass/neuralNetwork.java
+++ src/mainClass/neuralNetwork.java
@@ -1,115 +1,115 @@
 package mainClass;
 
 public class neuralNetwork {
-  Matrix I, H, O, Wih, Who, Bih, Bho;
-  double learningRate = 0.05;
+	Matrix I, H, O, Wih, Who, Bih, Bho;
+	double learningRate = 0.05;
 
-  public neuralNetwork(int inputNodes, int hiddenNodes, int outputNodes) {
-    this.I = new Matrix(inputNodes, 1);
-    this.H = new Matrix(hiddenNodes, 1);
-    this.O = new Matrix(outputNodes, 1);
-    this.Wih = new Matrix(hiddenNodes, inputNodes);
-    this.Who = new Matrix(outputNodes, hiddenNodes);
-    this.Bih = new Matrix(hiddenNodes, 1);
-    this.Bho = new Matrix(outputNodes, 1);
-  }
+	public neuralNetwork(int inputNodes, int hiddenNodes, int outputNodes) {
+		this.I = new Matrix(inputNodes, 1);
+		this.H = new Matrix(hiddenNodes, 1);
+		this.O = new Matrix(outputNodes, 1);
+		this.Wih = new Matrix(hiddenNodes, inputNodes);
+		this.Who = new Matrix(outputNodes, hiddenNodes);
+		this.Bih = new Matrix(hiddenNodes, 1);
+		this.Bho = new Matrix(outputNodes, 1);
+	}
 
-  public void setLearningRate(double lr){
-    this.learningRate = lr;
-  }
+	public void setLearningRate(double lr) {
+		this.learningRate = lr;
+	}
 
-  public void randomiseWeights(double lowest, double highest, int digitsAfterComma) {
-    this.Wih.randomise(lowest, highest, digitsAfterComma);
-    this.Who.randomise(lowest, highest, digitsAfterComma);
-    this.Bih.randomise(lowest, highest, digitsAfterComma);
-    this.Bho.randomise(lowest, highest, digitsAfterComma);
-  }
+	public void randomiseWeights(double lowest, double highest, int digitsAfterComma) {
+		this.Wih.randomise(lowest, highest, digitsAfterComma);
+		this.Who.randomise(lowest, highest, digitsAfterComma);
+		this.Bih.randomise(lowest, highest, digitsAfterComma);
+		this.Bho.randomise(lowest, highest, digitsAfterComma);
+	}
 
-  public double[] feedForward(double[] input) {
-    this.I.define(input);
-    this.H = Matrix.multiply(Wih, I);
-    this.H.add(this.Bih);
-    this.H.sigmoidEach();
-    this.O = Matrix.multiply(Who, H);
-    this.O.add(this.Bho);
-    this.O.sigmoidEach();
-    return this.O.arr[0];
-  }
+	public double[] feedForward(double[] input) {
+		this.I.define(input);
+		this.H = Matrix.multiply(Wih, I);
+		this.H.add(this.Bih);
+		this.H.sigmoidEach();
+		this.O = Matrix.multiply(Who, H);
+		this.O.add(this.Bho);
+		this.O.sigmoidEach();
+		return this.O.arr[0];
+	}
 
-   public double[] feedForwardDebug(double[] input, boolean onlyOutput) {
-    this.I.define(input);
-    this.H = Matrix.multiply(Wih, I);
-    this.H.add(this.Bih);
-    this.H.sigmoidEach();
-    this.O = Matrix.multiply(Who, H);
-    this.O.add(this.Bho);
-    this.O.sigmoidEach();
-    
-    if (!onlyOutput) {
-      System.out.println("Input");
-      this.I.debug();
-      System.out.println("Weights between Input and Hidden");
-      this.Wih.debug();
-      System.out.println("Biases between Input and Hidden");
-      this.Bih.debug();
-      System.out.println("Hidden");
-      this.H.debug();
-      System.out.println("Weights between Hidden and Output");
-      this.Who.debug();
-      System.out.println("Biases between Hidden and Output");
-      this.Bho.debug();
-    }
-    System.out.println("Output");
-    this.O.debug();
-    return this.O.arr[0];
-  }
+	public double[] feedForwardDebug(double[] input, boolean onlyOutput) {
+		this.I.define(input);
+		this.H = Matrix.multiply(Wih, I);
+		this.H.add(this.Bih);
+		this.H.sigmoidEach();
+		this.O = Matrix.multiply(Who, H);
+		this.O.add(this.Bho);
+		this.O.sigmoidEach();
 
-   public void backpropagation(double[] input, double[] expectedOutput) {
+		if (!onlyOutput) {
+			System.out.println("Input");
+			this.I.debug();
+			System.out.println("Weights between Input and Hidden");
+			this.Wih.debug();
+			System.out.println("Biases between Input and Hidden");
+			this.Bih.debug();
+			System.out.println("Hidden");
+			this.H.debug();
+			System.out.println("Weights between Hidden and Output");
+			this.Who.debug();
+			System.out.println("Biases between Hidden and Output");
+			this.Bho.debug();
+		}
+		System.out.println("Output");
+		this.O.debug();
+		return this.O.arr[0];
+	}
 
-    //feed forward
-    this.I.define(input);
-    this.H = Matrix.multiply(Wih, I);
-    this.H.add(this.Bih);
-    this.H.sigmoidEach();
-    this.O = Matrix.multiply(Who, H);
-    this.O.add(this.Bho);
-    this.O.sigmoidEach();
+	public void backpropagation(double[] input, double[] expectedOutput) {
 
-    //initialize matrices
-    Matrix EO = new Matrix(this.O.rows, 1);
-    //matrix outputErrors = new matrix(this.O.rows,1);
-    EO.define(expectedOutput);
+		// feed forward
+		this.I.define(input);
+		this.H = Matrix.multiply(Wih, I);
+		this.H.add(this.Bih);
+		this.H.sigmoidEach();
+		this.O = Matrix.multiply(Who, H);
+		this.O.add(this.Bho);
+		this.O.sigmoidEach();
 
-    //calculate output errors
-    Matrix outputErrors = Matrix.substract(EO, O);
+		// initialize matrices
+		Matrix EO = new Matrix(this.O.rows, 1);
+		// matrix outputErrors = new matrix(this.O.rows,1);
+		EO.define(expectedOutput);
 
-    //calculate the output gradient
-    Matrix outputGradient = this.O.dSigmoidEach();
-    outputGradient.multiply(outputErrors);
-    outputGradient.scl(this.learningRate);
+		// calculate output errors
+		Matrix outputErrors = Matrix.substract(EO, O);
 
-    //calculate the deltas of the outputHidden weights
-    Matrix WhoDeltas = Matrix.multiply(outputGradient, this.H.transpose());
+		// calculate the output gradient
+		Matrix outputGradient = this.O.dSigmoidEach();
+		outputGradient.multiply(outputErrors);
+		outputGradient.scl(this.learningRate);
 
-    //adjust the HO weights by the calculated deltas
-    this.Who.add(WhoDeltas);
-    //adjust the HO biases by its deltas (basically just the output gradient...)
-    this.Bho.add(outputGradient);
+		// calculate the deltas of the outputHidden weights
+		Matrix WhoDeltas = Matrix.multiply(outputGradient, this.H.transpose());
 
-    //calculate hidden errors
-    Matrix hiddenErrors = Matrix.multiply(this.Who.transpose(), outputErrors);
+		// adjust the HO weights by the calculated deltas
+		this.Who.add(WhoDeltas);
+		// adjust the HO biases by its deltas (basically just the output gradient...)
+		this.Bho.add(outputGradient);
 
-    //calculate the hidden gradient
-    Matrix hiddenGradient = this.H.dSigmoidEach();
-    hiddenGradient.multiply(hiddenErrors);
-    hiddenGradient.scl(this.learningRate);
+		// calculate hidden errors
+		Matrix hiddenErrors = Matrix.multiply(this.Who.transpose(), outputErrors);
 
-    //calculate the deltas of the inputHidden weights
-    Matrix WihDeltas = Matrix.multiply(hiddenGradient, this.I.transpose());
+		// calculate the hidden gradient
+		Matrix hiddenGradient = this.H.dSigmoidEach();
+		hiddenGradient.multiply(hiddenErrors);
+		hiddenGradient.scl(this.learningRate);
 
-    //adjust the IH weights by the calculated deltas
-    this.Wih.add(WihDeltas);
-    //adjust the IH biases by its deltas (basically just the output gradient...)
-    this.Bih.add(hiddenGradient);
-  }
+		// calculate the deltas of the inputHidden weights
+		Matrix WihDeltas = Matrix.multiply(hiddenGradient, this.I.transpose());
+
+		// adjust the IH weights by the calculated deltas
+		this.Wih.add(WihDeltas);
+		// adjust the IH biases by its deltas (basically just the output gradient...)
+		this.Bih.add(hiddenGradient);
+	}
 }
