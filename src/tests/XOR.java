a028fa89300c7b7e452c6f1d0e060ddfe95adef5
OwlSurojit <surojit@gmx.de>
2018-08-04 20:22:05 +0200

    editing XOR and referencing the processing core library
    The XOR example was written in processing with a Graphical Interface
    using a lot of the processing functions. And since it would have been
    quite some work to write a fully new version using only the standard
    JRE, I simply referenced core.jar of the processing library to the
    project.

--- src/tests/XOR.java
+++ src/tests/XOR.java
@@ -1,51 +1,70 @@
-neuralNetwork nn;
-final int precision = 10;
+package tests;
 
-void setup() {  
-  size(400, 400);
+import processing.core.*;
 
-  nn = new neuralNetwork(2, 8, 1);
-  nn.setLearningRate(0.1);
-  nn.randomiseWeights(0, 1, 2);
-  /*
-  nn.feedForwardDebug(new float[]{0, 0}, true);
-   nn.feedForwardDebug(new float[]{1, 0}, true);
-   nn.feedForwardDebug(new float[]{1, 1}, true);
-   nn.feedForwardDebug(new float[]{0, 1}, true);
-   */
-}
+import mainClass.neuralNetwork;
 
-final float rectSize = (width / precision);
+public class XOR extends PApplet {
 
-void draw() {
+	neuralNetwork nn;
+	final int precision = 10;
 
-  for (int i=0; i<1000; i++) {
-    switch(int(random(0, 4))) {
-    case 0: 
-      nn.backpropagation(new float[]{1, 0}, new float[]{1}); 
-      break;
-    case 1: 
-      nn.backpropagation(new float[]{0, 1}, new float[]{1}); 
-      break;
-    case 2: 
-      nn.backpropagation(new float[]{0, 0}, new float[]{0}); 
-      break;
-    default: 
-      nn.backpropagation(new float[]{1, 1}, new float[]{0});
-    }
-  }
+	public void setup() {
 
-  noStroke();
-  background(255);
-  for (int i = 0; i < width; i += rectSize) {
-    for (int j = 0; j < height; j += rectSize) {
-      float res = nn.feedForward(new float[]{i/(width-rectSize), j/(height-rectSize)})[0];
-      fill(res * 255);
-      rect(i, j, rectSize, rectSize);
-    }
-  }
-}
+		nn = new neuralNetwork(2, 8, 1);
+		nn.randomiseWeights(0, 1, 2);
+		/*
+		 * nn.feedForwardDebug(new double[]{0, 0}, true); nn.feedForwardDebug(new
+		 * double[]{1, 0}, true); nn.feedForwardDebug(new double[]{1, 1}, true);
+		 * nn.feedForwardDebug(new double[]{0, 1}, true);
+		 */
+	}
 
-void mousePressed() {
-  setup();
+	final float rectSize = width / precision;
+
+	public void draw() {
+
+		for (int i = 0; i < 1000; i++) {
+			switch (PApplet.parseInt(random(0, 4))) {
+			case 0:
+				nn.backpropagation(new double[] { 1, 0 }, new double[] { 1 });
+				break;
+			case 1:
+				nn.backpropagation(new double[] { 0, 1 }, new double[] { 1 });
+				break;
+			case 2:
+				nn.backpropagation(new double[] { 0, 0 }, new double[] { 0 });
+				break;
+			default:
+				nn.backpropagation(new double[] { 1, 1 }, new double[] { 0 });
+			}
+		}
+
+		noStroke();
+		background(255);
+		for (int i = 0; i < width; i += rectSize) {
+			for (int j = 0; j < height; j += rectSize) {
+				double res = nn.feedForward(new double[] { i / (width - rectSize), j / (height - rectSize) })[0];
+				fill((int) (res * 255));
+				rect(i, j, rectSize, rectSize);
+			}
+		}
+	}
+
+	public void mousePressed() {
+		setup();
+	}
+
+	public void settings() {
+		size(400, 400);
+	}
+
+	static public void main(String[] passedArgs) {
+		String[] appletArgs = new String[] { "tests.XOR" };
+		if (passedArgs != null) {
+			PApplet.main(concat(appletArgs, passedArgs));
+		} else {
+			PApplet.main(appletArgs);
+		}
+	}
 }
